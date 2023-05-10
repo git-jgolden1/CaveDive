1cf0a5933ccf5762dd1a2264b50ea8e1de34a42c
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-05-10 13:48:50 -0700

    added hole3 to evenDeeperLeftTurn

---
+++ level1/leftPath/deeperLeftPath/evenDeeperLeftPath/hole2.sh
@@ -0,0 +1,4 @@
+holeJump
+sleepyEcho "This hole is so shallow it almost doesn't even feel like a hole. However, you do notice something shiny by your feet."
+echo "It's a diamond!"
+pickUpItem "diamond"
